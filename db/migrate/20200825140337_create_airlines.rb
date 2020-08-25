829d2ee54035c07b33a57d4aee7b7b7ee9582dac
Sneha <snhthakare@gmail.com>
2020-08-25 19:41:31 +0530

    Adds airline and review model and associations

---
+++ db/migrate/20200825140337_create_airlines.rb
@@ -0,0 +1,10 @@
+class CreateAirlines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :airlines do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
