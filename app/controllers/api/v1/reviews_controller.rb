21bddece005332c055980ac5f2fb4e14f56ea6e0
Sneha <snhthakare@gmail.com>
2020-08-26 22:39:10 +0530

    Adds Review controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,25 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
+            def create
+                review = Review.find(params[:id])
+                if review.update(review_params)
+                    render json: ReviewSerializer.new(review).serialized_json
+                end
+            end
+
+            def destroy
+                review = Review.find(params[:id])
+                review.destroy
+                redirect_to root_path
+            end
+
+            private
+
+            def review_params
+                params.require(:review).permit(:title, :description, :score, :airline_id)
+            end
+
         end
     end
 end
