847a0867f0ad9e0e1b426f04ee1ee3941bb61f21
Sneha <snhthakare@gmail.com>
2020-08-27 19:11:02 +0530

    Gets airlines data list

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-    return (<h1>Airlines index view for app</h1>)
+    
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/v1/airlines.json')
+        .then(res =>  {setAirlines(res.data.data)})
+        .catch(res => console.log(res))
+
+    },[airlines.length])
+
+    const list = airlines.map(item => {
+    return(<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
+    
+    return (
+            <ul>{list}</ul>
+        )
 }
 
 export default Airlines;
